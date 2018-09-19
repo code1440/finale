e9598c40bcfb941c1dac6ce38860823cee5bb04e
code1440 <42753561+code1440@users.noreply.github.com>
2018-09-19 15:27:58 -0500

    Update wpprod.tf

--- lab03/WP/wpprod/wpprod.tf
+++ lab03/WP/wpprod/wpprod.tf
@@ -1,22 +1,22 @@
-resource "aws_placement_group" "prod" {
-  name     = "prod"
+resource "aws_placement_group" "test" {
+  name     = "test"
   strategy = "cluster"
 }
 
-resource "aws_autoscaling_group" "wpprod" {
-  name                      = "wp-terraform-prod"
+resource "aws_autoscaling_group" "bar" {
+  name                      = "foobar3-terraform-test"
   max_size                  = 5
   min_size                  = 2
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 4
   force_delete              = true
-  placement_group           = "${aws_placement_group.prod.id}"
-  launch_configuration      = "${aws_launch_configuration.wp_prod.name}"
-  vpc_zone_identifier       = ["subnet-00d193a4d3d04e6d9", "subnet-0c6b0649641900769"]
+  placement_group           = "${aws_placement_group.test.id}"
+  launch_configuration      = "${aws_launch_configuration.foobar.name}"
+  vpc_zone_identifier       = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
 
   initial_lifecycle_hook {
-    name                 = "wp_prod"
+    name                 = "foobar"
     default_result       = "CONTINUE"
     heartbeat_timeout    = 2000
     lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
@@ -45,22 +45,5 @@ EOF
     key                 = "lorem"
     value               = "ipsum"
     propagate_at_launch = false
-  }
-}
-
-resource "aws_launch_template" "foobar" {
-  name_prefix = "foobar"
-  image_id = "ami-0a950ca991c6c754b"
-  instance_type = "t2.micro"
-}
-
-resource "aws_autoscaling_group" "bar" {
-  availability_zones = ["us-east-1"]
-  desired_capacity = 1
-  max_size = 3
-  min_size = 1
-  launch_template = {
-    id = "${aws_launch_template.foobar.id}"
-    version = "$$Latest"
   }
 }
