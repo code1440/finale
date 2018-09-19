8f328fbb73ff68af24ca0f414077d782944fa8e2
code1440 <42753561+code1440@users.noreply.github.com>
2018-09-19 10:59:26 -0500

    Update bhprod.tf

--- lab03/bhprod/bhprod.tf
+++ lab03/bhprod/bhprod.tf
@@ -3,9 +3,9 @@ provider "aws" {
 region = "us-east-1"
  }
  resource "aws_security_group" "bh_rule" {
-  name        = "bh_rule"
+  name        = "bh_prod_rule"
   description = "Allow all inbound traffic"
-  vpc_id      = "vpc-08ccb45a1f7489582"
+  vpc_id      = "vpc-04186de44db3095fa"
 
   ingress {
     from_port   = 22
@@ -22,14 +22,14 @@ region = "us-east-1"
     
   }
 }
-resource "aws_instance" "BHDEV" {
+resource "aws_instance" "BHprod" {
   ami           = "ami-6871a115"
   instance_type = "t2.micro"
   vpc_security_group_ids = [
-        "${aws_security_group.bh_rule.id}"    ]
-  subnet_id = "subnet-0065df4d2343952b5"
+        "${aws_security_group.bh_prod_rule.id}"    ]
+  subnet_id = "subnet-00d193a4d3d04e6d9"
   key_name = "fullstack"
   tags {
-    Name = "BHDev"
+    Name = "BHprod"
   }
 }  
