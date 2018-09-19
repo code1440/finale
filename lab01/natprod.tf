a057eacc6eda275d2359d33421c70460e6abde07
code1440 <42753561+code1440@users.noreply.github.com>
2018-09-18 22:03:58 -0500

    Create natprod.tf

---
+++ lab01/natprod.tf
@@ -0,0 +1,22 @@
+# Create NAT instance in Prod subnet with Prod security Group
+provider "aws" {
+region = "us-east-1"
+ }
+resource "aws_instance" "NATProd" {
+  ami           = "ami-0422d936d535c63b1"
+  instance_type = "t2.micro"
+  security_groups = [
+        "sg-01006c8ba9d6d2bf3"
+    ]
+  subnet_id = "subnet-00d193a4d3d04e6d9"
+  key_name = "fullstack"
+  tags {
+    Name = "NATProd"
+  }
+}
+
+resource "aws_route" "r" {
+  route_table_id = "rtb-02ec857935c9afb7c"
+  destination_cidr_block    = "0.0.0.0/0"
+  instance_id = "${aws_instance.NATProd.id}"
+}
