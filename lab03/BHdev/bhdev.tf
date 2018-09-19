fab66c1d2be0dc9ae6ef472352cc9d9f8e253784
code1440 <42753561+code1440@users.noreply.github.com>
2018-09-19 10:47:20 -0500

    Update bhdev.tf

--- lab03/BHdev/bhdev.tf
+++ lab03/BHdev/bhdev.tf
@@ -2,8 +2,8 @@
 provider "aws" {
 region = "us-east-1"
  }
- resource "aws_security_group" "bastion_host_rule" {
-  name        = "allow_all"
+ resource "aws_security_group" "bh_rule" {
+  name        = "bh_rule"
   description = "Allow all inbound traffic"
   vpc_id      = "vpc-08ccb45a1f7489582"
 
@@ -25,12 +25,11 @@ region = "us-east-1"
 resource "aws_instance" "BHDEV" {
   ami           = "ami-6871a115"
   instance_type = "t2.micro"
-  security_groups = [
-        "bastion_host_rule"    ]
+  vpc_security_group_ids = [
+        "${aws_security_group.bh_rule.id}"    ]
   subnet_id = "subnet-0065df4d2343952b5"
   key_name = "fullstack"
   tags {
     Name = "BHDev"
   }
-}
-
+}  
